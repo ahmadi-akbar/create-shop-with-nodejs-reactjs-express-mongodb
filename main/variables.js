da84b09fa7421a17994677969642fc1e4c3c6759
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-06-14 19:29:06 +0430

    ds

---
+++ main/variables.js
@@ -0,0 +1,12 @@
+export default {
+  SERVER_PORT: 3001,
+  CLIENT_PORT: 3000,
+  VERSION_NUM: 0.0,
+  site_name: "Create shop with Node.js, Express, React.js, react-admin",
+  BASE_URL: "http://localhost:3001",
+  SHOP_URL: "http://localhost:3001/",
+  ADMIN_URL: "http://localhost:3000/",
+  FRONT_ROUTE: "http://localhost:3001/customer",
+  ADMIN_ROUTE: "http://localhost:3001/customer"
+
+};
