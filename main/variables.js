891f1bbfe739cb87091c378bdd4ce0beed6bccbb
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-06-16 15:04:23 +0430

    change wizard

--- main/variables.js
+++ main/variables.js
@@ -1,12 +1,12 @@
 export default {
-  "dbName": "name",
-  "mongodbConnectionUrl": "mongodb://127.0.0.1:27017",
-  "SERVER_PORT": "3001",
-  "CLIENT_PORT": "3002",
-  "site_name": "SITE NAME",
-  "BASE_URL": "http://yasiman.shop",
-  "SHOP_URL": "http://yasiman.shop/",
-  "ADMIN_URL": "http://admin.yasiman.shop",
-  "ADMIN_USERNAME": "admin",
-  "ADMIN_PASSWORD": "as12de3648"
+    "dbName": "name",
+    "mongodbConnectionUrl": "mongodb://127.0.0.1:27017",
+    "SERVER_PORT": "3001",
+    "CLIENT_PORT": "3002",
+    "site_name": "SITE NAME",
+    "BASE_URL": "http://localhost:3001",
+    "SHOP_URL": "http://localhost:3001/",
+    "ADMIN_URL": "http://localhost:3000",
+    "ADMIN_USERNAME": "admin",
+    "ADMIN_PASSWORD": "admin"
 }
