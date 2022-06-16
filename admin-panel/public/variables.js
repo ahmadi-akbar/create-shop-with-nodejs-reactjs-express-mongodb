0c65083826741beffdb089676a360ea9e9614754
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-06-16 15:10:57 +0430

    remove variables.js from admin-panel

--- admin-panel/public/variables.js
+++
@@ -1,7 +0,0 @@
-// window.BASE_URL='https://gameboss.shop';
-// window.SHOP_URL='https://gameboss.shop/';
-// window.ADMIN_ROUTE='https://gameboss.shop/admin';
-window.BASE_URL='http://localhost:3001';
-window.SHOP_URL='http://localhost:3001/';
-window.ADMIN_ROUTE='http://localhost:3001/admin';
-window.ADMIN_URL='http://localhost:3000';
