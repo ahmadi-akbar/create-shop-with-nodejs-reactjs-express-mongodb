4d2ba69b5a58cf93d316189f71ff0630adf5aa0a
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-06-16 14:26:03 +0430

    this update was heavy

--- main/src/client/assets/index.js
+++ main/src/client/assets/index.js
@@ -1,33 +1,45 @@
-import VARIABLE from "#v/variables";
+import CONFIG from "#config/config";
+import {isClient} from "../functions/index"
 
+// export const config = "../../..public_media/site_setting/"+ ("config.js");
+// export const url ="/site_setting/img/";
+export let url =CONFIG().SHOP_URL;
+if(isClient){
+  url="/site_setting/img/";
+}
+// if (env === 'development') {
+//   url =VARIABLE.SHOP_URL;
+//   console.log("VARIABLE",VARIABLE);
+//
+// }
 
-export const defaultImg = VARIABLE.SHOP_URL + ("not-found.png");
+// console.log("VARIABLE",VARIABLE//);
+export const defaultImg = url + ("not-found.png");
 
-export const SnapChatIcon = VARIABLE.SHOP_URL + ("snapchat.svg");
-export const logoImg = VARIABLE.SHOP_URL + "site_setting/" + ("logo.png") || VARIABLE.SHOP_URL + "site_setting/" + ("logo.jpg");
-export const payImg = VARIABLE.SHOP_URL + ("pay.png");
-export const enamadImg = VARIABLE.SHOP_URL + ("enamad.png");
-export const etmeImg = VARIABLE.SHOP_URL + ("etme.jpeg");
-export const etehadImg = VARIABLE.SHOP_URL + ("etehad.png");
-export const blueImg = VARIABLE.SHOP_URL + ("blue.png");
-export const topsaleImg = VARIABLE.SHOP_URL + ("topsale.png");
+export const SnapChatIcon = url + ("snapchat.svg");
+export const logoImg = url + "site_setting/" + ("logo.png") || url + "site_setting/" + ("logo.jpg");
+export const payImg = url + ("pay.png");
+export const enamadImg = url + ("enamad.png");
+export const etmeImg = url + ("etme.jpeg");
+export const etehadImg = url + ("etehad.png");
+export const blueImg = url + ("blue.png");
+export const topsaleImg = url + ("topsale.png");
 
-export const slide1Img = VARIABLE.SHOP_URL + ("im/slide1.jpg");
-export const slide2Img = VARIABLE.SHOP_URL + ("im/slide2.jpg");
-export const slide3Img = VARIABLE.SHOP_URL + ("im/slide3.jpg");
-export const slide4Img = VARIABLE.SHOP_URL + ("im/slide4.jpg");
-export const slide5Img = VARIABLE.SHOP_URL + ("im/slide5.jpg");
-export const valentineDays = VARIABLE.SHOP_URL + ("im/valentine-days.jpg");
-export const eid = VARIABLE.SHOP_URL + ("im/eid.jpg");
+export const slide1Img = url + ("im/slide1.jpg");
+export const slide2Img = url + ("im/slide2.jpg");
+export const slide3Img = url + ("im/slide3.jpg");
+export const slide4Img = url + ("im/slide4.jpg");
+export const slide5Img = url + ("im/slide5.jpg");
+export const valentineDays = url + ("im/valentine-days.jpg");
+export const eid = url + ("im/eid.jpg");
 // export const slideOffer1Img = path.resolve(__dirname, ".", "img/im/offer-1.png");
-export const slideOffer1Img = VARIABLE.SHOP_URL + "im/offer-1.png";
-export const slideOffer2Img = VARIABLE.SHOP_URL + ("im/offer-2.png");
-export const slideOffer3Img = VARIABLE.SHOP_URL + ("im/offer-3.png");
-export const slideOffer4Img = VARIABLE.SHOP_URL + ("im/offer-4.png");
-export const slideOffer5Img = VARIABLE.SHOP_URL + ("im/offer-5.png");
-export const slideOffer6Img = VARIABLE.SHOP_URL + ("im/offer-6.png");
-export const slideOffer7Img = VARIABLE.SHOP_URL + ("im/offer-7.png");
-export const slideOffer8Img = VARIABLE.SHOP_URL + ("im/offer-8.png");
-export const spriteImg = VARIABLE.SHOP_URL + ("icons-sprite_2x.png");
-export const config = VARIABLE.SHOP_URL + "site_setting/"+ ("config.js");
-// export const slideOffer5Img = VARIABLE.SHOP_URL+('im/offer-5.png');
+export const slideOffer1Img = url + "im/offer-1.png";
+export const slideOffer2Img = url + ("im/offer-2.png");
+export const slideOffer3Img = url + ("im/offer-3.png");
+export const slideOffer4Img = url + ("im/offer-4.png");
+export const slideOffer5Img = url + ("im/offer-5.png");
+export const slideOffer6Img = url + ("im/offer-6.png");
+export const slideOffer7Img = url + ("im/offer-7.png");
+export const slideOffer8Img = url + ("im/offer-8.png");
+export const spriteImg = url + ("icons-sprite_2x.png");
+// export const slideOffer5Img = url+('im/offer-5.png');
