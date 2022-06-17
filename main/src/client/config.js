e3b358ace2c4669f5810378b488aae3840302a40
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-06-17 15:50:45 +0430

    Update config.js

--- main/src/client/config.js
+++ main/src/client/config.js
@@ -3,10 +3,15 @@ const isSSR = typeof window === 'undefined';
 if (isSSR) var window = {};
 
 export default isSSR
-  ? {}
+  ? {
+    BASE_URL:'http://localhost:3001',
+    SHOP_URL:'http://localhost:3001/',
+    FRONT_ROUTE:'http://localhost:3001/customer'
+  }
   : {
       SHOP_URL: window.SHOP_URL,
       defaultImg: 'https://www.shop.com/not-found.png',
       SnapChatIcon: 'https://www.shop.com/snapchat.svg',
       BASE_URL: window.BASE_URL,
+      FRONT_ROUTE:window.FRONT_ROUTE
     };
